daab520acae72bb211c1f9f93079cf512abacd80
Reinaldo Tineo <rei.vzl@gmail.com>
2018-05-11 14:24:49 -0430

    V-0.9.0

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {InfoService} from "../../services/info.service";
 import {MessageService} from "../../services/message.service";
+import * as swal from 'sweetalert';
 
 @Component({
   selector: 'app-contact',
@@ -13,5 +14,11 @@ export class ContactComponent{
 				public _MessageService: MessageService){
 
 	}
+
+	contactForm(form) {
+ 		this._MessageService.sendMessage(form).subscribe(() => {
+ 			swal('Contact form', 'Message sent', 'success');
+ 		});
+ 	}
 
 }
