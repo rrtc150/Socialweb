2ed7fb304d5212ac3d8f05f1cdc10f87cbfe30ae
Reinaldo Tineo <rei.vzl@gmail.com>
2018-05-11 14:08:26 -0430

    V-0.9.2

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {InfoService} from "../../services/info.service";
+import {MessageService} from "../../services/message.service";
 
 @Component({
   selector: 'app-contact',
@@ -8,7 +9,8 @@ import {InfoService} from "../../services/info.service";
 
 export class ContactComponent{
 
-	constructor(public _is:InfoService){
+	constructor(public _is:InfoService,
+				public _MessageService: MessageService){
 
 	}
 
